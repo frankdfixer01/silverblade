bfb203740dbf62dfaaa7a363cc7aaa22d0bb8fa5
frankdfixer01 <33572896+frankdfixer01@users.noreply.github.com>
2017-11-11 21:25:48 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php 
+	require('db/functions.php');
+	include('controller/themechanger.php');
+	include('controller/controller.php');
+	include('template/header.php');
+    include('template/image-template.php');
+	require_once('template/modals.php');
+?>
+
+<div class="<?php echo $cont; ?>" style="padding-top:10px">
+		 <div class="w3-col m2 left-table w3-left <?php echo $color.' '; echo $edge.' '; echo $box; ?>" style="margin-right:10px;margin-left:0px">
+            <?php include('template/menu.php') ?>
+        </div>
+        <div class="w3-col m7">
+            <?php include('template/contact-template.php') ?>
+        </div>
+        <div class="w3-col m3 right-table w3-padding <?php echo $color.' '; echo $edge.' '; echo $box; ?> " style="margin-left:15px;width:22.5%">
+            <?php include('template/calendar.php') ?>
+        </div>
+</div>
+	
+	<?php include('template/footer.php'); ?>
+
+
+	
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+	require('db/functions.php');
+	include('controller/themechanger.php');
+	include('controller/controller.php');
+	include('template/header.php');
+    include('template/image-template.php');
+	require_once('template/modals.php');
+	include('template/index-template.php');
+	include('template/footer.php');
+	$date = date('Y-m-d');
+	
+	//promote_student(29, 13, '2017-12-26');
+	
+?>
+
+
+
+
+	
+
